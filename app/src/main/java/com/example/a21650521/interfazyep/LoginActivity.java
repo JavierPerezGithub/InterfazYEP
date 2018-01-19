8732bb078678b83a3beddaf050fbfdf4ac15b0fe
Javier <JavierPerezJaurena@gmail.com>
2018-01-19 14:06:33 +0100

    Initial commit

---
+++ app/src/main/java/com/example/a21650521/interfazyep/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.example.a21650521.interfazyep;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+private TextView mSignUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mSignUp = (TextView)findViewById(R.id.textDont);
+    }
+
+    public void onClick(View v){
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
+    }
+}
